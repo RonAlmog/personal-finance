8ecb7830fcd5b4f11d3c53a158c83d7eca732889
Ron Almog <ron8132@gmail.com>
2024-05-20 16:40:11 -0400

    add post endpoint, with zValidator

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,11 @@
 // books.ts
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
 
 const app = new Hono()
   .get(
@@ -27,7 +29,32 @@ const app = new Hono()
     }
   )
 
-  .post("/", (c) => c.json("create account", 201))
+  .post(
+    "/",
+    clerkMiddleware(),
+    // use the accouts schema, but only use specific fields
+    zValidator("json", insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const values = c.req.valid("json"); // values is typed!
+      const auth = getAuth(c);
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        // unless you use returning(), nothing will come back
+        .returning();
+
+      return c.json({ data }); // returning array for some reason
+      // return c.json({ data: data[0] });
+    }
+  )
   .get("/:id", (c) => c.json(`get ${c.req.param("id")}`));
 
 export default app;
--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -6,3 +7,7 @@ export const accounts = pgTable("accounts", {
   userId: text("user_id").notNull(),
   plaidId: text("plaid_id"),
 });
+
+// drizzle-zod is a plugin for Drizzle ORM that allows you to
+// generate Zod schemas from Drizzle ORM schemas.
+export const insertAccountSchema = createInsertSchema(accounts);
