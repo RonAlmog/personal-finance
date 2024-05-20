0c3cd67c60053767f865d7c401833e7f5358b2c5
Ron Almog <ron8132@gmail.com>
2024-05-19 21:49:17 -0400

    add react query, provider

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,20 @@
 // books.ts
+import { db } from "@/db/drizzle";
+import { accounts } from "@/db/schema";
 import { Hono } from "hono";
 
 const app = new Hono()
-  .get("/", (c) => c.json({ accounts: [] }))
+  .get("/", async (c) => {
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts);
+
+    return c.json({ data });
+  })
+
   .post("/", (c) => c.json("create account", 201))
   .get("/:id", (c) => c.json(`get ${c.req.param("id")}`));
 
