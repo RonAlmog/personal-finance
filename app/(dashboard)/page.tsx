13e8126d0cdf0e77e452b013523287bfd599609e
Ron Almog <ron8132@gmail.com>
2024-05-20 08:46:17 -0400

    use typesafe useAccounts query

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,19 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+"use client";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const { data: accounts, isLoading } = useGetAccounts();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Authenticated page
-      <UserButton afterSignOutUrl="/" />
-    </main>
+    <div>
+      {accounts?.map((account) => (
+        <div key={account.id}>{account.name}</div>
+      ))}
+    </div>
   );
 }
