367ae98d0f1c53bd98eb1f635273b181370a935a
Ron Almog <ron8132@gmail.com>
2024-05-21 20:09:08 -0400

    add datatable, with pagination

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { Plus } from "lucide-react";
+import { Payment, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
 
-type Props = {};
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  // ...
+];
 
-const AccountsPage = (props: Props) => {
+const AccountsPage = () => {
   const newAccount = useNewAccount();
   return (
-    <div>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg: justify-between">
           <CardTitle className="text-xl line-clamp-1">Accounts page</CardTitle>
@@ -18,6 +28,9 @@ const AccountsPage = (props: Props) => {
             Add New
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
       </Card>
     </div>
   );
