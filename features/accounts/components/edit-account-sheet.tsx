831985147237aab133301291c718d500f6030de2
Ron Almog <ron8132@gmail.com>
2024-05-23 10:44:09 -0400

    complete edit account

--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -9,10 +9,11 @@ import {
 import AccountForm from "./account-form";
 import { insertAccountSchema } from "@/db/schema";
 import { z } from "zod";
-import { useCreateAccount } from "../api/use-create-account";
 import { useOpenAccount } from "../hooks/use-open-accounts";
+
 import { useGetAccount } from "../api/use-get-account";
 import { Loader2 } from "lucide-react";
+import { useEditAccount } from "../api/use-edit-account";
 
 const formSchema = insertAccountSchema.pick({ name: true });
 
@@ -22,11 +23,14 @@ const EditAccountSheet = () => {
   const { isOpen, onOpen, onClose, id } = useOpenAccount();
 
   const accountQuery = useGetAccount(id!);
-  const mutation = useCreateAccount();
+
+  const editMutation = useEditAccount(id);
+
+  const isPending = accountQuery.isPending;
   const isLoading = accountQuery.isLoading;
 
   const onSubmit = (values: FormValues) => {
-    mutation.mutate(values, {
+    editMutation.mutate(values, {
       onSuccess: () => {
         onClose();
       },
@@ -54,7 +58,7 @@ const EditAccountSheet = () => {
           <AccountForm
             id={id}
             onSubmit={onSubmit}
-            disabled={mutation.isPending}
+            disabled={isPending}
             defaultValues={defaultValues}
           />
         )}
