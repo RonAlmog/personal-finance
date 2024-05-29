777951f0d024e922babcbf146735b6b7075b8ef7
Ron Almog <ron8132@gmail.com>
2024-05-29 16:34:07 -0400

    add transaction form

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -5,6 +5,7 @@ import NewAccountSheet from "@/features/accounts/components/new-account-sheet";
 import EditAccountSheet from "@/features/accounts/components/edit-account-sheet";
 import NewCategorySheet from "@/features/categories/components/new-category-sheet";
 import EditCategorySheet from "@/features/categories/components/edit-category-sheet";
+import NewTransactionSheet from "@/features/transactions/components/new-transaction-sheet";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -16,6 +17,7 @@ export const SheetProvider = () => {
       <EditAccountSheet />
       <NewCategorySheet />
       <EditCategorySheet />
+      <NewTransactionSheet />
     </>
   );
 };
