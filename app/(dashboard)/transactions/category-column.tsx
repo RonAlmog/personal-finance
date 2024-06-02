5959d43656f4071ec73169b01709b8e50578f4fc
Ron Almog <ron8132@gmail.com>
2024-06-02 19:33:34 -0400

    handle uncategorized case

--- app/(dashboard)/transactions/category-column.tsx
+++ app/(dashboard)/transactions/category-column.tsx
@@ -1,4 +1,5 @@
 import { useOpenCategory } from "@/features/categories/hooks/use-open-category";
+import { useOpenTransaction } from "@/features/transactions/hooks/use-open-transaction";
 import { cn } from "@/lib/utils";
 import { TriangleAlert } from "lucide-react";
 
@@ -10,9 +11,12 @@ type Props = {
 
 const CategoryColumn = ({ id, category, categoryId }: Props) => {
   const { onOpen: onOpenCategory } = useOpenCategory();
+  const { onOpen: onOpenTransaction } = useOpenTransaction();
   const onClick = () => {
     if (categoryId) {
       onOpenCategory(categoryId);
+    } else {
+      onOpenTransaction(id);
     }
   };
   return (
