299161da97c7151b9341878d94c60fccbb0d7347
Ron Almog <ron8132@gmail.com>
2024-06-02 19:20:44 -0400

    fix action col for transactions

--- app/(dashboard)/transactions/actions.tsx
+++ app/(dashboard)/transactions/actions.tsx
@@ -8,19 +8,20 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
-import { useOpenAccount } from "@/features/accounts/hooks/use-open-accounts";
-import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+
 import { useConfirm } from "@/hooks/use-confirm";
+import { useOpenTransaction } from "@/features/transactions/hooks/use-open-transaction";
+import { useDeleteTransaction } from "@/features/transactions/api/use-delete-transaction";
 
 type Props = { id: string };
 
 const Actions = ({ id }: Props) => {
-  const deleteMutation = useDeleteAccount(id);
+  const deleteMutation = useDeleteTransaction(id);
   const [ConfirmDialog, confirm] = useConfirm(
     "Are you sure?",
-    "You are about to delete this account"
+    "You are about to delete this transaction"
   );
-  const { onOpen } = useOpenAccount();
+  const { onOpen } = useOpenTransaction();
 
   const handleDelete = async () => {
     const ok = await confirm();
