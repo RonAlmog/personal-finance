f2fd857508114c39ab87f92cf735d89bf94fa9ce
Ron Almog <ron8132@gmail.com>
2024-06-07 10:16:55 -0400

    add datagrid, datacard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,23 +1,9 @@
-"use client";
+import DataGrid from "@/components/data-grid";
 
-import { Button } from "@/components/ui/button";
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { UserButton } from "@clerk/nextjs";
-
-export default function Home() {
-  const { data: accounts, isLoading } = useGetAccounts();
-  const { onOpen } = useNewAccount();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+export default function DashboardPage() {
   return (
-    <div>
-      <Button onClick={onOpen}>Add account</Button>
-      {accounts?.map((account) => (
-        <div key={account.id}>{account.name}</div>
-      ))}
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
