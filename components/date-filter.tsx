8a66d5ea9cca62521b854dec602a24f184c67609
Ron Almog <ron8132@gmail.com>
2024-06-13 19:59:50 -0400

    filter transactions by date range

--- components/date-filter.tsx
+++ components/date-filter.tsx
@@ -14,6 +14,8 @@ import {
   PopoverTrigger,
   PopoverClose,
 } from "./ui/popover";
+import { useState } from "react";
+import { Calendar } from "./ui/calendar";
 
 const DateFilter = () => {
   const router = useRouter();
@@ -30,6 +32,29 @@ const DateFilter = () => {
     from: from ? new Date(from) : defaultFrom,
     to: to ? new Date(to) : defaultTo,
   };
+  const [date, setDate] = useState<DateRange | undefined>(paramState);
+
+  const pushToUrl = (dateRange: DateRange | undefined) => {
+    const query = {
+      from: format(dateRange?.from || defaultFrom, "yyyy-MM-dd"),
+      to: format(dateRange?.to || defaultTo, "yyyy-MM-dd"),
+      accountId,
+    };
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query,
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  };
+
+  const onReset = () => {
+    setDate(undefined);
+    pushToUrl(undefined);
+  };
 
   return (
     <Popover>
@@ -41,8 +66,42 @@ const DateFilter = () => {
           className="lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition"
         >
           <span>{formatDateRange(paramState)}</span>
+          <ChevronDown className="mr-2 size-4 opacity-50" />
         </Button>
       </PopoverTrigger>
+      <PopoverContent className="lg:w-auto w-full p-0" align="start">
+        <Calendar
+          disabled={false}
+          initialFocus
+          mode="range"
+          defaultMonth={date?.from}
+          selected={date}
+          onSelect={setDate}
+          numberOfMonths={2}
+        />
+        <div className="p-4 w-full flex items-center gap-x-2">
+          <PopoverClose asChild>
+            <Button
+              onClick={onReset}
+              disabled={!date?.from || !date?.to}
+              className="w-full"
+              variant="outline"
+            >
+              Reset
+            </Button>
+          </PopoverClose>
+          <PopoverClose asChild>
+            <Button
+              onClick={() => pushToUrl(date)}
+              disabled={!date?.from || !date?.to}
+              className="w-full"
+              variant="default"
+            >
+              Apply
+            </Button>
+          </PopoverClose>
+        </div>
+      </PopoverContent>
     </Popover>
   );
 };
